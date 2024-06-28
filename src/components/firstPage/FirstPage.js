86757e9d52ac3e0dbae70e0611458694cd74442f
Valeriya <lerakorolkova88@gmail.com>
2024-06-28 16:47:59 +1200

    modified image

--- src/components/firstPage/FirstPage.js
+++ src/components/firstPage/FirstPage.js
@@ -9,10 +9,10 @@ function FirstPage(){
     return(
         <Container fluid className="p-0 first-page-container" >
             <Row className="first-container">
-                <Col lg={6} xl={4} className="text-center img-container">
+                <Col sm={12} md={6} className="text-center img-container">
                         <img src={plakhina} alt="Daria Plakhina" className="img-fluid mb-3 img-plakhina"/>
                 </Col>
-                <Col lg={6} xl={8} className="d-flex flex-column justify-content-center align-items-center quote-first-page-container">
+                <Col sm={12} md={6} className="d-flex flex-column justify-content-center quote-first-page-container">
                         <h1 className="quote-first-page cormorant-medium">“Мне нравится вкладывать силы и время в развитие учеников”</h1>
                     <div className="btn-join">
                        <ButtonOne />
